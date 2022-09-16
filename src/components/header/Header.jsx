0773cd590ad9a7f8996adca8e040d50c20de7001
7hemant <hemantmourya127@gmail.com>
2022-09-16 17:12:19 +0530

    responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
+import { FaBars } from "react-icons/fa";
 const Header = () => {
+  const [toggle, settoggle] = useState("close");
+  const toggleHandler = () => {
+    toggle === "close" ? settoggle("open") : settoggle("close");
+    console.log("click", toggle);
+  };
   return (
     <header>
-      <nav>
+      <div className="icon">
+        <FaBars onClick={toggleHandler} />
+      </div>
+      <nav className={`${toggle}`}>
         <ul>
           <li>Works</li>
           <li>Blog</li>
